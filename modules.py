e2a6e8ee169586c30d028eac11f800c661d0ef2a
sharanya3184 <sharanya3184@gmail.com>
2025-06-19 19:08:35 +0530

    modules py code modified

--- modules.py
+++ modules.py
@@ -4,3 +4,11 @@ from pymongo import MongoClient
 
 MONGO_URL = os.environ.get("MONGO_URL")
 DB_NAME = os.environ.get("DB_NAME", "Textile")
+
+
+client = MongoClient(MONGO_URL)
+db = client[DB_NAME]
+users_collection = db['users']
+products_collection = db['products']
+categories_collection = db['categories']
+
